d835729aceadfbc56196dbffc0db009e413ad299
Limitex <76650151+Limitex@users.noreply.github.com>
2023-03-01 04:31:37 +0900

    Add what you need for development

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const Process = require('child_process');
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -11,7 +12,14 @@ const createWindow = () => {
     }
   });
   mainWindow.loadFile('index.html');
+
+  // DEBUG
+  mainWindow.webContents.openDevTools(/*{ mode: 'detach' }*/);
 };
 
 app.once('ready', createWindow);
 app.once('window-all-closed', app.quit);
+
+Process.exec('python --version', (error, stdout, stderr) => {
+  console.log(error ?? stdout ?? stderr);
+});
