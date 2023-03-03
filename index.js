671af0b1ca5cb18402211918a17a368df02c47d9
Limitex <76650151+Limitex@users.noreply.github.com>
2023-03-03 13:08:50 +0900

    Separate the load window code

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const Process = require('child_process')
-const psTree = require('ps-tree')
-const path = require('path')
+const { app, ipcMain } = require('electron')
 const iwm = require('./index_w_main.js');
 const server = require('./index_server.js');
+const iwl = require('./index_w_load.js');
 
-const PORT = 8000
-const HOST = 'localhost'
-const WORK = 'py_src.main:app'
+let loadWindowObj = null;
+app.once('ready', () => loadWindowObj = iwl.loadWindow());
 
 std_data = []
 std_status = -1
-let loadWindowObj = null;
-const loadWindow = () => {
-  loadWindowObj = new BrowserWindow({ 
-    width: 600, 
-    height: 400, 
-    frame: false,
-    skipTaskbar: true,
-    resizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload_load.js'),
-    },
-  })
-  loadWindowObj.setAlwaysOnTop(true)
-  ipcMain.handle('stdout', () => std_data)
-  ipcMain.handle('stdstatus', () => std_status)
-  ipcMain.handle('exitAll', () => {
-    loadWindowObj.close();
-    app.once('window-all-closed', app.quit);
-  })
-  loadWindowObj.loadFile('window_load/index.html');
-}
-app.once('ready', loadWindow);
+ipcMain.handle('stdout', () => std_data)
+ipcMain.handle('stdstatus', () => std_status)
+ipcMain.handle('exitAll', () => {
+  loadWindowObj.close();
+  app.once('window-all-closed', app.quit);
+})
 
 server.StartServer(
   (std) => {
---
+++ index_w_load.js
@@ -0,0 +1,22 @@
+const { BrowserWindow } = require("electron");
+const path = require("path");
+
+const loadWindow = () => {
+  const loadWindowObj = new BrowserWindow({
+    width: 600,
+    height: 400,
+    frame: false,
+    skipTaskbar: true,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload_load.js"),
+    },
+  });
+  loadWindowObj.setAlwaysOnTop(true);
+  loadWindowObj.loadFile("window_load/index.html");
+  return loadWindowObj
+};
+
+module.exports = {
+  loadWindow: loadWindow,
+};
