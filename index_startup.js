355bc810701f89bb44f05c7d33e7d7762c2edf86
Limitex <76650151+Limitex@users.noreply.github.com>
2023-03-06 21:10:19 +0900

    Add python install function

--- index_startup.js
+++ index_startup.js
@@ -1,30 +1,73 @@
-const { app } = require('electron');
-const path = require('path');
-const SavePath = path.join(app.getPath('userData'), 'python');
+const { app } = require("electron");
+const path = require("path");
 const Process = require("child_process");
+const https = require("https");
+const fs = require("fs");
+const unzipper = require("unzipper");
+const { createDirectoryByOverwrite } = require("./index_file.js");
 
-const PythonURI = 'https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip'
-const PythonPIPURI = 'https://bootstrap.pypa.io/get-pip.py'
+const SavePath = app.getPath("userData");
+const TmpPath = path.join(SavePath, "tmp");
+const PythonPath = path.join(SavePath, "python", "python.exe");
+const PythonDirPath = path.join(SavePath, "python");
 
+const PythonURI = "https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip";
+const PythonPIPURI = "https://bootstrap.pypa.io/get-pip.py";
 
 const checkPython = (callback) => {
-  const pythonPath = path.join(SavePath, 'python.exe');
-  Process.exec(pythonPath + ' --version', (error, stdout, stderr) => {
-    callback(stdout.includes('Python 3.10'));
+  Process.exec(PythonPath + " --version", (error, stdout, stderr) => {
+    callback(stdout.includes("Python 3.10"));
   });
 };
 
 const checkPythonModules = (callback) => {
-  callback(true)
-}
+  callback(true);
+};
 
 const installPython = (callback) => {
-  callback(true)
-}
+  createDirectoryByOverwrite(TmpPath);
+  createDirectoryByOverwrite(PythonDirPath);
+
+  const zipPath = path.join(TmpPath, "python.zip");
+  const pipPath = path.join(TmpPath, "get-pip.py");
+
+  https.get(PythonURI, (response) => {
+    const file = fs.createWriteStream(zipPath);
+    response.pipe(file);
+    file.on("finish", () => {
+      file.close();
+      fs.createReadStream(zipPath)
+        .pipe(unzipper.Extract({ path: PythonDirPath }))
+        .on("finish", () => {
+          https.get(PythonPIPURI, (response) => {
+            const pfile = fs.createWriteStream(pipPath);
+            response.pipe(pfile);
+            Process.exec(
+              PythonPath + " " + pipPath,
+              (error, stdout, stderr) => {
+                const pthPath = path.join(PythonDirPath, "python310._pth");
+                fs.readFile(pthPath, "utf8", (err, data) => {
+                  if (err) throw err;
+                  const updatedData = data.replace(
+                    /#import site/g,
+                    "import site"
+                  );
+                  fs.writeFile(pthPath, updatedData, "utf8", (err) => {
+                    if (err) throw err;
+                    callback(true);
+                  });
+                });
+              }
+            );
+          });
+        });
+    });
+  });
+};
 
 const installModules = (callback) => {
-  callback(true)
-}
+  callback(true);
+};
 
 module.exports = {
   checkPython: checkPython,
