23d6f9ee4478e4689b0dee2a74f4f99019cdf6ae
Limitex <76650151+Limitex@users.noreply.github.com>
2023-03-14 21:14:10 +0900

    code formatting (window_load)

--- window_load/src/main.js
+++ window_load/src/main.js
@@ -4,9 +4,9 @@ const id = setInterval(async function () {
   const status = await window.background.stdstatus();
   if (result.length !== lastResult) {
     console.log(result.length + result[result.length - 1]);
-    const logTextConteiner = document.getElementById("log-text-container")
-    const logText = document.getElementById("log-text")
-    logText.innerHTML = result.join('<br>');
+    const logTextConteiner = document.getElementById("log-text-container");
+    const logText = document.getElementById("log-text");
+    logText.innerHTML = result.join("<br>");
     lastResult = result.length;
     logTextConteiner.scrollTop = logText.scrollHeight;
   }
@@ -29,5 +29,5 @@ const id = setInterval(async function () {
 }, 1);
 
 const exit_func = () => {
-  window.background.exitAll()
-}
+  window.background.exitAll();
+};
