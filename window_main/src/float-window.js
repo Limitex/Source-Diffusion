2b766e1f7c937c4b405958a0197fcc9df36db673
Limitex <76650151+Limitex@users.noreply.github.com>
2023-03-30 00:25:45 +0900

    Show floatWindow after iframe finishes loading

--- window_main/src/float-window.js
+++ window_main/src/float-window.js
@@ -1,7 +1,14 @@
+const iframeContents = document.getElementById("float-window-contents");
+
+iframeContents.addEventListener('load', () => {
+  if (iframeContents.contentDocument.readyState === 'complete') {
+    document.getElementById("float-window-container").style = "";
+  }
+});
+
 const showFloatWindow = (title, htmlPath) => {
-  document.getElementById("float-window-container").style = "";
   document.getElementById("float-top-bar-title").innerText = title;
-  document.getElementById("float-window-contents").src = htmlPath;
+  iframeContents.src = htmlPath;
 };
 
 const closeFloatWindow = () => {
