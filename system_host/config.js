f5baf3e1941460073f069e71757d6b301183f932
Limitex <76650151+Limitex@users.noreply.github.com>
2023-04-05 23:30:25 +0900

    Make config compatible with other OS

--- system_host/config.js
+++ system_host/config.js
@@ -1,5 +1,6 @@
 const { app } = require("electron");
 const path = require("path");
+const os = require('os');
 
 const isDev = process.argv.some(arg => arg === '--dev');
 const isDevLoad = process.argv.some(arg => arg === '--lw-dev');
@@ -12,13 +13,23 @@ const WORK = "py_src.main:app";
 
 const SavePath = app.getPath("userData");
 const TmpPath = path.join(SavePath, "tmp");
-const PythonPath = path.join(SavePath, "python", "python.exe");
 const PythonDirPath = path.join(SavePath, "python");
-const PipPath = path.join(PythonDirPath, "Scripts", "pip.exe");
-
-const PythonURI =
-  "https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip";
 const PythonPIPURI = "https://bootstrap.pypa.io/get-pip.py";
+
+let PythonPath
+let PipPath
+let PythonURI
+
+if (os.platform() === 'win32') {
+  PythonPath = path.join(SavePath, "python", "python.exe");
+  PipPath = path.join(PythonDirPath, "Scripts", "pip.exe");
+  PythonURI = "https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip";
+} else if (os.platform() === 'darwin' || os.platform() === 'linux') {
+  PythonPath = path.join(SavePath, "python", "python");
+  PipPath = path.join(PythonDirPath, "Scripts", "pip");
+  PythonURI = "https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tar.xz";
+}
+
 
 module.exports = {
   isDev: isDev,
