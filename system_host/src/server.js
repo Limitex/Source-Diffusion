ca23bdebdff9cd43a1ebdc0aaa5be7145d7048b1
Limitex <76650151+Limitex@users.noreply.github.com>
2023-04-13 17:29:38 +0900

    Fixed possible MaxBufferError

--- system_host/src/server.js
+++ system_host/src/server.js
@@ -2,21 +2,16 @@ const { app } = require("electron");
 const Process = require("child_process");
 const psTree = require("ps-tree");
 const config = require("../config.js");
+const { spawn } = require('child_process');
 
 const StartServer = (stdout, stderr, exited) => {
   process.chdir(config.AppDir);
-  const ServerProcess = Process.exec(
-    config.PythonPath +
-      " -m uvicorn --host " +
-      config.HOST +
-      " --port=" +
-      config.PORT +
-      " --workers 1 " +
-      config.WORK
-  );
-  ServerProcess.stdout.on("data", stdout);
-  ServerProcess.stderr.on("data", stderr);
-  ServerProcess.on("close", exited);
+  const ServerProcess = spawn(config.PythonPath, [
+    "-m", "uvicorn", "--host", config.HOST, 
+    "--port=" + config.PORT, "--workers", "1", config.WORK]);
+  ServerProcess.stdout.on('data', (data) => stdout(`${data}`));
+  ServerProcess.stderr.on('data', (data) => stderr(`${data}`));
+  ServerProcess.on('close', exited);
 
   pids = [];
   psTree(ServerProcess.pid, (err, children) =>
