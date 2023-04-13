6519e35f59387072c3d53c753114d9d8951f7c7f
Limitex <76650151+Limitex@users.noreply.github.com>
2023-04-13 17:29:25 +0900

    Fixed possible MaxBufferError

--- system_host/src/startup.js
+++ system_host/src/startup.js
@@ -3,9 +3,10 @@ const config = require("../config.js");
 const { spawn } = require('child_process');
 
 const checkEnvironment = (outputCallback, normalCallback, abnormalCallback) => {
-  const process = Process.exec(`\"${config.CheckScriptPath}\" \"${config.AppDir}\" \"${config.AppDataDir}\"`, (error, stdout, stderr) => {});
-  process.stdout.on("data", (data) => outputCallback(data.trimEnd()));
-  process.on("close", (data) => {
+  const process = spawn(config.CheckScriptPath, [config.AppDir, config.AppDataDir]);
+  process.stdout.on('data', (data) => outputCallback(`${data}`));
+  process.stderr.on('data', (data) => outputCallback(`${data}`));
+  process.on('close', (data) => {
     if (data == 0) {
       normalCallback();
     } else {
