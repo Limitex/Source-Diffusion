75e4b5ff16f12b85742916fdcac260bf26e45084
Limitex <76650151+Limitex@users.noreply.github.com>
2023-04-13 18:31:15 +0900

    Create a script to check Python

--- system_dependent/mac/py_check.sh
+++ system_dependent/mac/py_check.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-# TODO : Add check pythons
+APP_PATH=$1
+APP_DATA_PATH=$2
+
+PYTHON_PATH="$APP_DATA_PATH/python"
+
+if ! "$PYTHON_PATH/bin/python3" --version >/dev/null 2>&1; then
+  exit 1
+fi
+
+error=$($PYTHON_PATH/bin/pip3 freeze -r "$APP_PATH/py_src/requirements.txt" 2>&1 | grep WARNING)
+if [ -n "$error" ]; then
+  exit 1
+fi
 
 exit 0
