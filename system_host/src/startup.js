50c848b2b58a440ec02de01f06b026b001f6b10d
Limitex <76650151+Limitex@users.noreply.github.com>
2023-04-13 17:15:09 +0900

    Fixed a problem that does not start with MaxBufferError

--- system_host/src/startup.js
+++ system_host/src/startup.js
@@ -1,5 +1,6 @@
 const Process = require("child_process");
 const config = require("../config.js");
+const { spawn } = require('child_process');
 
 const checkEnvironment = (outputCallback, normalCallback, abnormalCallback) => {
   const process = Process.exec(`\"${config.CheckScriptPath}\" \"${config.AppDir}\" \"${config.AppDataDir}\"`, (error, stdout, stderr) => {});
@@ -14,9 +15,10 @@ const checkEnvironment = (outputCallback, normalCallback, abnormalCallback) => {
 }
 
 const installEnvironment = (outputCallback, normalCallback, abnormalCallback) => {
-  const process = Process.exec(`\"${config.InstallScriptPath}\" \"${config.AppDir}\" \"${config.AppDataDir}\"`, (error, stdout, stderr) => {});
-  process.stdout.on("data", (data) => outputCallback(data.trimEnd()));
-  process.on("close", (data) => {
+  const process = spawn(config.InstallScriptPath, [config.AppDir, config.AppDataDir]);
+  process.stdout.on('data', (data) => outputCallback(`${data}`));
+  process.stderr.on('data', (data) => outputCallback(`${data}`));
+  process.on('close', (data) => {
     if (data == 0) {
       normalCallback();
     } else {
