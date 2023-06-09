5fea32bef8f7c4ea926e0c2a80bb7851beed2d9b
Limitex <76650151+Limitex@users.noreply.github.com>
2023-06-09 18:02:23 +0900

    Create placeholders for save location settings

---
+++ window_main/src/settings.js
@@ -0,0 +1,5 @@
+const fullSaveSetting = () =>{
+    const InputPath = document.getElementById("input-full-save-path");
+    const InputEnable = document.getElementById("input-full-save-switch").checked;
+    console.log(InputEnable)
+}
